da12f13a3fadde40ebc679863f8c4143d3e11433
helloworldms <72796025+helloworldms@users.noreply.github.com>
2020-11-29 23:57:20 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TOC from "./components/TOC";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// 클래스 소속시 function 생략
+// 하나의 최상위 태그
+// attribute = props
+
+class Contents extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" Sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Contents
+          title="HTML"
+          desc="HTML is HyperText Markup Language."
+        ></Contents>
+      </div>
+    );
+  }
 }
 
 export default App;
